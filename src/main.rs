ea34c0b255d811776860770877000e3a4d9799b0
Alejandro Calzadilla <alejandrocalzadilla@gmail.com>
2022-10-05 19:32:01 -0400

    practice with rust variables

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
 fn main() {
     println!("Hello, world!");
+    let mut x:u32 = 4;
+    println!("x is : {}", x);
+    x = 5;
+    println!("x is : {}", x);
+
+
+    // potential risk as I am recreating a variable in memory with same name.
+    let m = 3;
+    println!("m is : {}", m);
+    let m = 4;
+    println!("m is : {}", m);
+
+    // scope
+     {
+        // different scope out of layer outside in main
+        // I can use a variable outside of this scope without it affecting the outside.
+        let x = 2;
+        println!("x is : {}", x);
+
+     }
+
+    //  constants
+    const SECONDS_IN_MINUTE: u32 = 60;
+    println!("SECONDS: {}", SECONDS_IN_MINUTE);
+
+
 }
